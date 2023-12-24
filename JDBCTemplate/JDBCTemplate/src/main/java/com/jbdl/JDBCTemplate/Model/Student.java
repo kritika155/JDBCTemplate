5ab7e542f7733318a420c138958cb37e63cc08bd
Kritika Roy <roykritika00@gmail.com>
2023-12-24 21:37:03 +0530

    jdbctemplate

---
+++ JDBCTemplate/JDBCTemplate/src/main/java/com/jbdl/JDBCTemplate/Model/Student.java
@@ -0,0 +1,57 @@
+package com.jbdl.JDBCTemplate.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter	
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    private Integer studId;
+
+    private String studName;
+
+    private String department;
+
+    private Double marks;
+
+	public Integer getStudId() {
+		return studId;
+	}
+
+	public void setStudId(Integer studId) {
+		this.studId = studId;
+	}
+
+	public String getStudName() {
+		return studName;
+	}
+
+	public void setStudName(String studName) {
+		this.studName = studName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public Double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Double marks) {
+		this.marks = marks;
+	}
+    
+}
